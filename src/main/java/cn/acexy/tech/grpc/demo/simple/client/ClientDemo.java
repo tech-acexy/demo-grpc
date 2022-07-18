686cb24e3b06419c32ed922f3fbff1e8002b0d92
Acexy <acexy@acexy.cn>
2022-07-18 20:25:31 +0800

    init client demo

--- src/main/java/cn/acexy/tech/grpc/demo/simple/client/ClientDemo.java
+++ src/main/java/cn/acexy/tech/grpc/demo/simple/client/ClientDemo.java
@@ -1,10 +1,12 @@
 package cn.acexy.tech.grpc.demo.simple.client;
 
 import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class ClientDemo {
 
-//    private final ManagedChannel managedChannel;
+    static ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",9981).usePlaintext().build();
+
 
     public static void main(String[] args) {
 

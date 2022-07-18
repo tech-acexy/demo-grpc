a65cf2ce1c935932538179963c689cccfb9b2baa
Acexy <acexy@acexy.cn>
2022-07-18 16:26:34 +0800

    init demo

---
+++ src/main/java/cn/acexy/tech/grpc/demo/simple/client/ClientDemo.java
@@ -0,0 +1,12 @@
+package cn.acexy.tech.grpc.demo.simple.client;
+
+import io.grpc.ManagedChannel;
+
+public class ClientDemo {
+
+//    private final ManagedChannel managedChannel;
+
+    public static void main(String[] args) {
+
+    }
+}
